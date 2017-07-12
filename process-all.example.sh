46ffcc2a1d39020306abf3f974930b80185a426b
Damian Schwyrz <mail@damianschwyrz.de>
2017-07-12 20:35:37 +0200

    readme, cleaer, stopwords

--- process-all.example.sh
+++ process-all.example.sh
@@ -1,10 +1,16 @@
 SAVEDMODEL_QUERY="suchbegriff"
 
+# 1. extract top 100 from google
 python3 tool.py scrape "$SAVEDMODEL_QUERY"
 
+# 2. extract content 
 python3 tool.py extract "$SAVEDMODEL_QUERY" 100 500
 
-python3 tool.py prepare "$SAVEDMODEL_QUERY" 3 2
+# 3. clean, content, remove stopwords, ...
+python3 tool.py prepare "$SAVEDMODEL_QUERY" 3 2 100
 
+# 4. train models and save
 python3 tool.py train "$SAVEDMODEL_QUERY" 165
 
+# INFO: Do not use this chain over and over, use the appropriate command from the list above!
+# For most cases you dont need to scrape google over and over again for the same query ;)
--- show-results.example.sh
+++ show-results.example.sh
@@ -2,8 +2,11 @@ SAVEDMODEL_QUERY="begriff"
 
 QUERY="keyword zum gegenprüfen, kann auch das gleiche wie savedmodel_query sein"
 
+# 1. Extract top topics
 python3 tool.py top-topics "$SAVEDMODEL_QUERY"
 
+# 2. Get the best document in trained models for query
 python3 tool.py best-doc "$SAVEDMODEL_QUERY" "$QUERY"
 
+# 3. Get the distances between every document
 python3 tool.py distances "$SAVEDMODEL_QUERY"
